d793cc14d8872659812d9cbb149fe9915fc74170
kibirige <kibirigejohn005@gmail.com>
2023-02-10 14:43:56 +0300

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :user_name, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 8 }
+    validates :role, presence: true
 end
