6646c02e51a03e35f4c0b195e53c5637e4c8d637
kibirige <kibirigejohn005@gmail.com>
2023-02-11 04:05:33 +0300

    update the index, show, destroy and update end points for a teacher

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,14 +3,17 @@ class TeachersController < ApplicationController
 
   # GET /teachers
   def index
-    @teachers = Teacher.all
-
-    render json: @teachers
+    @teachers = Teacher.all.includes(:user)
+    modified_teachers = @teachers.map do |teacher| 
+      combined_teacher_user(teacher, teacher.user)
+    end
+    
+    render json: modified_teachers
   end
 
   # GET /teachers/1
   def show
-    render json: @teacher
+    render json: combined_teacher_user(@teacher, @teacher.user)
   end
 
   # POST /teachers
@@ -33,16 +36,26 @@ class TeachersController < ApplicationController
 
   # PATCH/PUT /teachers/1
   def update
-    if @teacher.update(teacher_params)
-      render json: @teacher
-    else
+
+    @user = User.find(@teacher.user_id)
+    if @user.user_name != teacher_params[:user_name]
+      @user.update_column(:user_name, teacher_params[:user_name])
+    end
+
+    if @teacher.update(full_name: teacher_params[:full_name], sex: teacher_params[:sex], contact: teacher_params[:contact], email: teacher_params[:email], user_id: @user.id, physical_address: teacher_params[:physical_address], joining_date: teacher_params[:joining_date]) 
+      render json: combined_teacher_user(@teacher, @teacher.user), status: :ok
+    else 
       render json: @teacher.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /teachers/1
   def destroy
+    @user = User.find(@teacher.user_id)
     @teacher.destroy
+    @user.destroy
+
+    render json: { message: 'Teacher deleted successfully' }
   end
 
   private
@@ -60,13 +73,14 @@ class TeachersController < ApplicationController
       {
         id: teacher.id,
         user_name: user.user_name,
-        password: user.password,
         full_name: teacher.full_name,
         contact: teacher.contact,
         email: teacher.email,
         physical_address: teacher.physical_address,
         sex: teacher.sex,
         joining_date: teacher.joining_date,
+        created_at: teacher.created_at,
+        updated_at: teacher.updated_at
       }
     end
 end
