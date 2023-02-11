4e2e566d12d7d10d079be791c4a3818afe9cddf4
kibirige <kibirigejohn005@gmail.com>
2023-02-11 17:50:50 +0300

    add validations for name and level

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,2 +1,4 @@
 class Stream < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :level, presence: true, inclusion: { in: %w[o a] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :streams
   post '/login', to: 'users#login'
   resources :admins, only: [:create]
   resources :teachers
   resources :parents
   resources :students
+  resources :streams
 end
