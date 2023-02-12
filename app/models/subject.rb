d86a7712267397642d441f0482485a13999c4284
kibirige <kibirigejohn005@gmail.com>
2023-02-12 12:52:06 +0300

    create subject_teacher model and setup the right associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -4,4 +4,6 @@ class Subject < ApplicationRecord
     validates :level, presence: true
 
     has_many :exams
+    has_many :subject_teachers, dependent: :destroy
+    has_many :teachers, through: :subject_teachers
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,6 @@
 class Teacher < ApplicationRecord
   belongs_to :user
+
+  has_many :subject_teachers, dependent: :destroy
+  has_many :subjects, through: :subject_teachers
 end
