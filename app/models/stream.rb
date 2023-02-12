a0da9c21e27e540631abc8cdc582b25d8d055034
kibirige <kibirigejohn005@gmail.com>
2023-02-12 22:40:12 +0300

    add associations to the stream and student class that links them

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,4 +1,7 @@
 class Stream < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :level, presence: true, inclusion: { in: %w[o a] }
+
+    has_many :student_streams, dependent: :destroy
+    has_many :students, through: :student_streams
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,7 @@ class Student < ApplicationRecord
 
   has_many :subject_students, dependent: :destroy
   has_many :subjects, through: :subject_students
+
+  has_many :student_streams, dependent: :destroy
+  has_many :streams, through: :student_streams
 end
